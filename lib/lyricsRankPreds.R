a0d5bb48cada7dea13e4a1f1dfcba8e2af7860cb
Chang Liu <cl3388@columbia.edu>
2016-11-18 11:52:35 -0500

    Update lyricsRankPreds.R

--- lib/lyricsRankPreds.R
+++ lib/lyricsRankPreds.R
@@ -5,31 +5,32 @@ load("C:/Users/LIU/Desktop/ADS/P4/clustRank.RData")
 label <- lyrclust[,2]
 dat <- cbind(songs,label)
 
-ind <- c("duration","end_of_fade_in","key","key_confidence","loudness","mode",
-         "mode_confidence","start_of_fade_out","tempo","time_signature","time_signature_confidence","track_id","label")
-train <- dat[ind]
-model_train <- subset(train,select = -track_id)
-
+train_dat <- dat[,-1]
+rownames(train_dat) <- c(1:2350)
+train_dat <- as.data.frame(train_dat)
+train_dat <- train_dat[-c(715,950,991,1112,1325,1375,1658,1705,2284,2300),] #remove non-numeric rows
 #####################load your test data here############################
-test_dat <- train[1:100,-13] 
+load("C:/Users/LIU/Desktop/ADS/P4/test_dat.RData")
+rownames(test_dat) <- c(1:100)
+test_dat <- as.data.frame(test_dat)
 #########################################################################
 
 library(class)
 
 ###########################cv training process###########################
 cross_validation=function(features,labels,k,p){
-  dat=cbind(features,labels)
+  #dat=cbind(features,labels)
   accuracy=vector()
-  n=2350/k
+  n=2340/k
   index=list()
   for (i in 1:k){
-    index[[i]]=seq(i,2350,k)
+    index[[i]]=seq(i,2340,k)
   }
   for (i in 1:k){
     ind=index[[i]]
-    train_dat=dat[-ind,]
+    train_dat=features[-ind,]
     train_label=labels[-ind]
-    test_dat=dat[ind,]
+    test_dat=features[ind,]
     test_label=labels[ind]
     pred=knn(train_dat,test_dat,train_label,k=p)
     
@@ -51,17 +52,19 @@ gen_knn=function(features,labels){
   final.p=p[ind]
   return(final.p)
 }
-cv_k <- gen_knn(model_train[,-12],model_train$label)
+cv_k <- gen_knn(train_dat[,-16],train_dat$label)
 #######################################################################
 
 ######################predict rank in test data########################
-preds <- knn(model_train[,-12],test_dat[,-12],model_train$label,k=cv_k)
+preds <- knn(train_dat[,-16],test_dat[,-1],train_dat$label,k=cv_k)
 lyrRank <- data.frame()
 predRank <- data.frame()
-for(i in 1:length(test_dat$track_id)){
+for(i in 1:length(test_dat$files)){
   j <- preds[i]
   r <- matrix(clustRank[j,],ncol=length(clustRank[j,]))
-  lyrRank <- cbind(test_dat$track_id[j],r)
+  lyrRank <- cbind(test_dat$files[i],r)
   predRank <- rbind(predRank,lyrRank)
 }
-names(predRank) <- c('id',colnames(clustRank))
+names(predRank) <- c('files',colnames(clustRank))
+
+write.csv(predRank,"C:/Users/LIU/Desktop/ADS/P4/predRank.csv")
